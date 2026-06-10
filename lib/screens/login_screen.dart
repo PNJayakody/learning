775271f4ad63793805b1a1385382064bbe3c5d32
Pawani <pawaniniruddhika05@gmail.com>
2026-06-10 16:10:55 +0530

    testing

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -194,3 +194,12 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Test')));
+  }
+}
