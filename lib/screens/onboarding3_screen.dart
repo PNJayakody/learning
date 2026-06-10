6a6e164b36e133654b263c187ff7a334da4c74a2
Pawani <pawaniniruddhika05@gmail.com>
2026-06-10 15:39:18 +0530

    fixed minor imperfections in navigation

--- lib/screens/onboarding3_screen.dart
+++ lib/screens/onboarding3_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../theme/app_colors.dart';
 import '../theme/app_text_style.dart';
+import 'onboarding1_screen.dart';
 
 class Onboarding3 extends StatelessWidget {
   const Onboarding3({super.key});
@@ -104,7 +105,7 @@ class Onboarding3 extends StatelessWidget {
                               context,
 
                               MaterialPageRoute(
-                                builder: (context) => Onboarding3(),
+                                builder: (context) => Onboarding1(),
                               ),
                             );
                           },
